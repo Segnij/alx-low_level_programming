fb312f9f94f5fae39b12334f94f0a705c1c3ce2b
Segni <sengij@gmail.com>
2022-09-09 19:23:14 -0700

    final commit

---
+++ 0x00-hello_world/101-qoute.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+*main- Entry point
+*Discription: prints "and that piece of art is useful" - Dora Korpar, 2015-10-1*9"
+*Return: 1
+*/
+int main(void)
+{
+putchar("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+return (1) }
