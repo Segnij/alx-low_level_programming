d479f7c30a1a61ca2d53f090fc61b160d80183f9
Segni <sengij@gmail.com>
2022-09-09 19:39:53 -0700

    final commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <unistd.h>
+#include<unistd.h>
 /**
 *main- Entry point
 *Discription: prints "and that piece of art is useful" - Dora Korpar, 2015-10-1*9"
@@ -6,5 +6,5 @@
 */
 int main(void)
 {
-putchar("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
 return (1) };
