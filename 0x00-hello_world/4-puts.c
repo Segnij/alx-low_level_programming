d450880507662034a69efb6a4aa4bb23eb5345a5
Segni <sengij@gmail.com>
2022-09-09 17:38:20 -0700

    initial

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+/**
+ *main - Entry point
+ *Discription prints "\"Programming is like building a multilingual puzzle"
+
+ * Return: 0
+ */
+int main ()
+{
+  puts("Programming is like building a multilingual puzzle/n");
+    return(0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ *main - Entry point
+ *Discription - Prints "with proper grammar , but the outcome is a piece of art,"
+ *Return = 0
+ */
+int main ()
+{
+  printf("with proper grammar , but the outcome is a piece of art,/n");
+  return(0);
+}
