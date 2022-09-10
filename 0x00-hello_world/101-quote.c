9e77b036049ce3220f41f326b2e8f58e37f2a1d2
SEGNI <110995033+Segnij@users.noreply.github.com>
2022-09-10 05:46:26 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,15 @@
 #include<unistd.h>
 /**
 *main- Entry point
-*Discription: prints "and that piece of art is useful" - Dora Korpar, 2015-10-1*9"
+*Discription: prints "and that piece of art is useful" \
+- Dora Korpar, 2015-10-1*9"
 *Return: 1
 */
 int main(void)
 {
 write(2,
-      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-      59);
+"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+59);
 return (1);
 }
 
