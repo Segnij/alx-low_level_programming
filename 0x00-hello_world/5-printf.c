0dc17d9067207ad0d74a8ab0830911177331436f
Segni <sengij@gmail.com>
2022-09-09 18:15:14 -0700

    try

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 /**
  *main - Entry point
- *Discription - Prints "with proper grammar , but the outcome is a piece of art,"
- *Return = 0
+ *Discription: Prints "with proper grammar, but the outcome is a piece of art,"
+ *Return: 0
  */
 int main(void)
 {
-printf("with proper grammar , but the outcome is a piece of art,\n");
+  printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
 return (0); }
